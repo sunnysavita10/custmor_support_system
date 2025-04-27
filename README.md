68d7cbd9c681942c1b012043ee23ba102eaf98d9
sunnysavita10 <engsunny3365@gmail.com>
2025-04-27 22:06:15 +0530

    readme updated with the fastapi command

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ```
  conda create -p env python=3.10 -y
 ```
-
 ```
 conda activate env
+```
+```
+uvicorn main:app --reload --port 8001
 ```
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+
