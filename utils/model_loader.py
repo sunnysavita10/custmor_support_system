13ffa53bcb037516b33293de834bd99efbb01ffc
sunnysavita10 <engsunny3365@gmail.com>
2025-05-18 21:57:07 +0530

    code updated

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from config.config_loader import load_config
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
 
 class ModelLoader:
     """
@@ -17,7 +18,11 @@ class ModelLoader:
         """
         Validate necessary environment variables.
         """
-        required_vars = ["GOOGLE_API_KEY"]
+        required_vars = ["GOOGLE_API_KEY","GROQ_API_KEY"]
+        # Force clear old key if cached
+        # if "GROQ_API_KEY" in os.environ:
+        #     del os.environ["GROQ_API_KEY"]
+        self.groq_api_key=os.getenv("GROQ_API_KEY")
         missing_vars = [var for var in required_vars if not os.getenv(var)]
         if missing_vars:
             raise EnvironmentError(f"Missing environment variables: {missing_vars}")
@@ -36,7 +41,8 @@ class ModelLoader:
         """
         print("LLM loading...")
         model_name=self.config["llm"]["model_name"]
-        gemini_model=ChatGoogleGenerativeAI(model=model_name)
+        print(self.groq_api_key)
+        gemini_model=ChatGroq(model=model_name,api_key=self.groq_api_key)
+        #gemini_model=ChatGoogleGenerativeAI(model=model_name)
         
         return gemini_model  # Placeholder for future LLM loading
-
